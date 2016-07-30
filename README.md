21f6d1bc3975a0a235cba7b5fee93701ecb8ca24
siushen <784843867@qq.com>
2016-07-30 18:07:34 +0800

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 #RxJava Demo
+##相关
+https://github.com/ReactiveX/RxJava
+https://github.com/androidmalin/RengwuxianRxjava
--- app/src/main/java/com/xs/rxjavademo/MainActivity.java
+++ app/src/main/java/com/xs/rxjavademo/MainActivity.java
@@ -1,13 +1,35 @@
 package com.xs.rxjavademo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<File> folders = new ArrayList<>();
+        File file = new File("sddsdsd.jpg");
+        File file2 = new File("dssdsd.jpg");
+        folders.add(file);
+        folders.add(file2);
+
+        for (File folder :
+                folders) {
+            File[] f = folder.listFiles();
+            for (File ff :
+                    f) {
+                Log.e(TAG, "onCreate: "+ff.getName());
+
+            }
+        }
     }
 }
